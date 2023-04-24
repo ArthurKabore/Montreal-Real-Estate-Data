239da267360115f6d8828dca4ba9f1327a28c73a
ArthurKabore <arthur_travail@hotmail.fr>
2023-04-24 12:57:00 -0400

    Descriptive data scrap functional

--- condo_info_scraper.py
+++ condo_info_scraper.py
@@ -3,57 +3,76 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.common.by import By
 import time
 
 # TODO Read all 4,5k condo summaries to fetch deeper info.
 
 def read_page_row(response):
     soup = BeautifulSoup(response, "html.parser")
-    title_data = soup.findAll("div", {"class":"row"})
     room_data = soup.findAll("div", {"class":"row teaser"})
     specifics_data = soup.findAll("div", {"class":"col-lg-3 col-sm-6 carac-container"})
     
+    bedroom = ""
+    bathroom = ""
     content = []
 
     content.append(soup.find("h2", itemprop="address").string)
     content.append(soup.find("span", id="BuyPrice").string)
     
     for value in specifics_data[1:3]:
-        content.append(value.select_one(".carac-value span").string)
+        sub_value = value.select_one(".carac-value span")
+        
+        if sub_value:
+            content.append(sub_value.string)
 
     for data in room_data:
-        content.append(data.find("div", {"class":"col-lg-3 col-sm-6 piece"}).string.strip())
-        content.append(data.find("div", {"class":"col-lg-3 col-sm-6 cac"}).string.strip())
-        content.append(data.find("div", {"class":"col-lg-3 col-sm-6 sdb"}).string.strip())
-        
+        bathroom = data.find("div", {"class":"col-lg-3 col-sm-6 sdb"})
+        bedroom = data.find("div", {"class":"col-lg-3 col-sm-6 cac"})
+        rooms = data.find("div", {"class":"col-lg-3 col-sm-6 piece"})
+
+        if bathroom:
+            content.append(bathroom.string.strip())
+
+        if bedroom:
+            content.append(bedroom.string.strip())
+
+        if rooms:
+            content.append(rooms.string.strip())
+
     return content
 
 option_argument = Options()
 option_argument.add_argument("--window-size=1920,1080")
 
 driver = webdriver.Chrome(options = option_argument)
-driver.get('https://www.centris.ca/en/condos~for-sale~montreal-ville-marie/28624180?view=Summary')
+driver.get('https://www.centris.ca/en/condos~for-sale~montreal-verdun-ile-des-soeurs/20598619?view=Summary')
 
 print(read_page_row(driver.page_source))
 
-""" data = []
+data = []
 limit = 0
+time.sleep(15)
 
 #testing with limited fetching
-while limit  < 227:
-    try:
-        time.sleep(1)
-        data.extend(read_page(driver.page_source))
-        driver.find_element("xpath", '//*[@id="divWrapperPager"]/ul/li[4]/a').click()
+while limit < 100:
+    time.sleep(1)
+    data.append(read_page_row(driver.page_source))
 
-        limit += 1
+    try:
+        driver.find_element(By.CLASS_NAME, "next").click()
     except ElementClickInterceptedException:
         time.sleep(2)
-        driver.find_element("xpath", '/html/body/div/div/div[2]').click()
-        print("Popup blocked")
+        driver.find_element(By.CLASS_NAME, "DialogInsightLightBoxCloseButton").click()
+
+        print("Bye bye pop-up")
         time.sleep(2)
+
+    limit += 1
     print(limit)
 
 print(data, len(data), limit)
 
-np.savetxt('condo_price_data.csv', data, delimiter="\n", fmt="%s") """
+with open("output.csv", "w", encoding='utf-8') as txt_file:
+    for line in data:
+        txt_file.write(" ".join(line) + "\n")
