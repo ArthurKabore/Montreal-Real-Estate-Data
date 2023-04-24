108ece682e431566bd9cb2be7f294f2a2ac3e2b0
ArthurKabore <arthur_travail@hotmail.fr>
2023-04-23 20:01:12 -0400

    cleaner data ingestion for price fetching

--- centris_data_scrapping.py
+++ centris_data_scrapping.py
@@ -1,26 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import time
 
 url = "https://www.centris.ca/en/condos~for-sale~montreal-island?view=Thumbnail&uc=4"
 
 def read_page(response):
     soup = BeautifulSoup(response, "html.parser")
+    data = []
 
     for price in soup.findAll("div", {"class":"price"}):
-        print(price)
+        data.append(price.select_one(".price span").string)
 
-driver = webdriver.Chrome()
+    return data
+
+
+option_argument = Options()
+option_argument.add_argument("--window-size=1920,1080")
+
+driver = webdriver.Chrome(options = option_argument)
 driver.get('https://www.centris.ca/en/condos~for-sale~montreal-island?view=Thumbnail&uc=4')
 
-while True:
-    print("################################")
+data = []
+limit = 0
 
-    read_page(driver.page_source)
-    
-    time.sleep(3)
-
+#testing with limited fetching
+while limit  < 100:
+    print(read_page(driver.page_source))
+    # data.extend(read_page(driver.page_source))
     driver.find_element("xpath", '//*[@id="divWrapperPager"]/ul/li[4]/a').click()
 
+    limit += 1
+
+print(data, len(data), limit)
     
