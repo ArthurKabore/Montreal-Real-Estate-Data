073978f5cf65edc514caabe5de14e720e1b53766
ArthurKabore <arthur_travail@hotmail.fr>
2023-04-23 23:13:45 -0400

    Data collection function and edge cases handled

--- centris_data_scrapping.py
+++ centris_data_scrapping.py
@@ -1,7 +1,9 @@
 import requests
+import numpy as np
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import ElementClickInterceptedException
 import time
 
 url = "https://www.centris.ca/en/condos~for-sale~montreal-island?view=Thumbnail&uc=4"
@@ -26,12 +28,20 @@ data = []
 limit = 0
 
 #testing with limited fetching
-while limit  < 100:
-    print(read_page(driver.page_source))
-    # data.extend(read_page(driver.page_source))
-    driver.find_element("xpath", '//*[@id="divWrapperPager"]/ul/li[4]/a').click()
+while limit  < 227:
+    try:
+        time.sleep(1)
+        data.extend(read_page(driver.page_source))
+        driver.find_element("xpath", '//*[@id="divWrapperPager"]/ul/li[4]/a').click()
 
-    limit += 1
+        limit += 1
+    except ElementClickInterceptedException:
+        time.sleep(2)
+        driver.find_element("xpath", '/html/body/div/div/div[2]').click()
+        print("Popup blocked")
+        time.sleep(2)
+    print(limit)
 
 print(data, len(data), limit)
-    
+
+np.savetxt('condo_price_data.csv', data, delimiter="\n", fmt="%s")
